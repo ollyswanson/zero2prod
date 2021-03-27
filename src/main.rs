c02d2d00c902ad37aa3e0cec6f9e31500fd60c93
Olly Swanson <olly.swanson95@gmail.com>
2021-03-27 16:25:19 +0100

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+//! src/main.rs
+use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Panic if we can't read the configuration.
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+
+    run(listener)?.await
 }
